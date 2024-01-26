3275a8c44064e8d659c568f9e82c8990a5eaca13
kavyanshyt11 <152184800+kavyanshyt11@users.noreply.github.com>
2024-01-26 20:35:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("My first console project");
+
+            var first_name = "MS";
+            var last_name = " Dhoni";
+
+
+            var full_name =  first_name.concat(last_name);
+            console.log(full_name);
+
+            var a = 10;
+            var b = 5;
+
+            var add = a + b  ;
+            console.log(add);
+
+            var sub = a - b  ;
+            console.log(sub);
+
+            var multiple =  a * b ;
+            console.log(multiple);
+
+            var divide = a / b ;
+            var vs =  " Click on Create a new project to start a new development project Choose the project type (e.g. Console App Web Ap, etc.) and the programming language"
+            console.log(vs);
+           
